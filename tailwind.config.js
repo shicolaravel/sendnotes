d0c5d4f85ae55b5981e5ce9a1f4be00d5fbd171f
Shicosoft <shicoweb@gmail.com>
2024-08-27 10:18:42 +0300

    as

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,13 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-
+        "./vendor/wireui/wireui/src/*.php",
+        "./vendor/wireui/wireui/ts/**/*.ts",
+        "./vendor/wireui/wireui/src/WireUi/**/*.php",
+        "./vendor/wireui/wireui/src/Components/**/*.php",
+    ],
+    presets: [
+        require("./vendor/wireui/wireui/tailwind.config.js")
     ],
     darkMode: 'class',
     theme: {
